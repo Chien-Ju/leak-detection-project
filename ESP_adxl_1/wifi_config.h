429ccafd1e6b709ac9c2d471658273f40d59e3b2
Chien-Ju <skyler20001118@gmail.com>
2025-04-07 13:52:52 +0800

    add wifi_config.h under ESP_zdxl_1

---
+++ ESP_adxl_1/wifi_config.h
@@ -0,0 +1,18 @@
+#ifndef WIFI_CONFIG_H
+#define WIFI_CONFIG_H
+
+#define WIFI_SSID       "ChienJu"
+#define WIFI_PASSWORD   "jyd726489"
+#define SERVER_IP       "172.17.95.176"
+
+// #define WIFI_SSID       "61-4f"
+// #define WIFI_PASSWORD   "Qq0981673893"
+// #define SERVER_IP       "192.168.68.54"
+
+// #define WIFI_SSID       "Alchoholic_2G"
+// #define WIFI_PASSWORD   "aSej29)siE%q"
+// #define SERVER_IP       "192.168.50.80"
+
+#define SERVER_PORT     8888
+
+#endif
